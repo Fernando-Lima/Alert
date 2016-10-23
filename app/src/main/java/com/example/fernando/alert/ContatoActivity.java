23871576f0a47b53c718fdc0a42dd928003f9d88
Fernando-Lima <fernandosupweb@gmail.com>
2016-10-23 21:40:25 -0200

    Menu para excluir contato selecionado

--- app/src/main/java/com/example/fernando/alert/ContatoActivity.java
+++ app/src/main/java/com/example/fernando/alert/ContatoActivity.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -56,6 +59,25 @@ public class ContatoActivity extends DebugActivity{
     }
 
     @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(0,0,0,"Excluir");
+        menu.add(0,1,0,"Adicionar como contato principal");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case 0:
+                ContextMenu.ContextMenuInfo info = item.getMenuInfo();
+                int pos = ((AdapterView.AdapterContextMenuInfo)info).position;
+                dao.deletarContato(String.valueOf(contatos.get(pos).getId()));
+                atualizaLista();
+        }
+        return true;
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         atualizaLista();
     }
@@ -66,4 +88,5 @@ public class ContatoActivity extends DebugActivity{
         contatos.addAll(c);
         contatoListAdapter.notifyDataSetChanged();
     }
+
 }
